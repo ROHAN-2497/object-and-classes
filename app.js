62ec44104f9b1f1e297dc68a925d3df599951c80
Rohan_Islam <rohanislam3669@gmail.com>
2024-05-31 03:15:50 +0600

    callback

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
-function getData(dataId) {
+function getData(dataId, nextData) {
   setTimeout(() => {
-    console.log("hello", dataId);
+    console.log("data", dataId);
+    if (nextData) {
+      nextData();
+    }
   }, 2000);
 }
+getData(1, () => {
+  getData(2, () => {
+    getData(3);
+  });
+});
